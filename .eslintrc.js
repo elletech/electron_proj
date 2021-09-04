3f4f3c6c48ddb1c111faabe0e94ee30a4c20c7bc
elletech <mygit_yutasugao20171126@yahoo.co.jp>
2021-09-04 17:53:33 +0900

    complete prettier eslint husky

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true, //ブラウザ関連のglobal変数はeslintの対象から除外する
+  },
+  extends: [
+    'eslint:recommended', // defaultのルールを使用
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    // セミコロンをつける
+    semi: ['error', 'always'],
+    'semi-spacing': ['error', { after: true, before: false }],
+    'semi-style': ['error', 'last'],
+    'no-extra-semi': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  globals: {
+    JSX: true,
+  }
+};
