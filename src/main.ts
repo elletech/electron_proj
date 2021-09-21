6080e39d1f27010a92fad2b5ba58714295e0d71e
elletech <mygit_yutasugao20171126@yahoo.co.jp>
2021-09-21 20:27:43 +0900

    create calendar

--- src/main.ts
+++ src/main.ts
@@ -4,28 +4,35 @@ import { searchDevtools } from 'electron-search-devtools';
 
 const isDev = process.env.NODE_ENV === 'development';
 
-const execPath =
-  process.platform === 'win32'
-    ? '../node_modules/electron/dist/electron.exe'
-    : '../node_modules/.bin/electron';
+// const execPath =
+//   process.platform === 'win32'
+//     ? '../node_modules/electron/dist/electron.exe'
+//     : '../node_modules/.bin/electron';
 
 // 開発モードの場合はホットリロードする
-if (isDev) {
-  require('electron-reload')(__dirname, {
-    electron: path.resolve(__dirname, execPath),
-    forceHardReset: true,
-    hardResetMethod: 'exit',
-  });
-}
+// if (isDev) {
+//   require('electron-reload')(__dirname, {
+//     electron: path.resolve(__dirname, execPath),
+//     forceHardReset: true,
+//     hardResetMethod: 'exit',
+//   });
+// }
 
 // BrowserWindow インスタンスを作成する関数
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
+    // ウィンドウ作成時のオプション
     webPreferences: {
       preload: path.resolve(__dirname, 'preload.js'),
     },
+    width: 1000,
+    height: 1000,
+    transparent: true, // ウィンドウの背景を透過
+    frame: false, // 枠の無いウィンドウ
+    resizable: false, // ウィンドウのリサイズを禁止
+    hasShadow: false, //残像が残らないようにする
   });
-
+  mainWindow.setAlwaysOnTop(false); // 常に最背面
   if (isDev) {
     // 開発モードの場合はデベロッパーツールを開く
     mainWindow.webContents.openDevTools({ mode: 'detach' });
--- src/web/index.tsx
+++ src/web/index.tsx
@@ -1,12 +1,108 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 
 import './index.css';
 
+import moment from 'moment';
+
 const App = (): JSX.Element => {
+  const currentMonth = 0;
+
+  const currentDate = moment().add(currentMonth, 'months');
+
+  // 現在表示している月が何日まであるかを算出
+  const numOfMonth = currentDate.endOf('month').date();
+  // この月の1日〜最終日までの配列
+  const daysOfMonth = [...Array(numOfMonth).keys()].map(i => ++i);
+  // 1日の曜日（0~6の数値で取得）日->0, 月->1, 火->2, 水->3, 木->4, 金->5, 土->6
+  const firstWeekDay = currentDate.startOf('month').weekday();
+
+  // 週ごとの二次元配列を生成
+  const data = [...Array(6)].map((empty, weekIndex) =>
+    [...Array(7)].map((empty, dayIndex) => {
+      const i = 7 * weekIndex + dayIndex - firstWeekDay;
+      if (i < 0 || daysOfMonth[i] === undefined) {
+        return null;
+      }
+      return daysOfMonth[i];
+    }),
+  );
+
+  // 全てnullの配列があれば除去する
+  const calendarData = data.filter(
+    week => week.filter(day => day != null).length > 0,
+  );
+
+  // 現在表示している月の前の月が何日まであるかを算出
+  const copyCurrentDate = moment().add(currentMonth, 'months');
+
+  const lastMonthDate = copyCurrentDate.subtract(1, 'months');
+  const numOfDaysBeforeMonth = lastMonthDate.endOf('month').date();
+
+  // 最初の週の前の月が表示されている日数を算出
+  const numOfLastMonthDays = calendarData[0].filter(data => {
+    return data == null;
+  }).length;
+
+  // 最終週の前の月が表示されている日数を算出
+  const numOfNextMonthDays = calendarData[calendarData.length - 1].filter(
+    data => {
+      return data == null;
+    },
+  ).length;
+
+  //カレンダーの配列に先月の日付と来月の日付を加える
+  const completeCalendarData = calendarData.map((week, index) => {
+    // 最初の週は前の月の日付を入れる
+    if (index == 0) {
+      let copyWeek = _.cloneDeep(week);
+      for (let i = 0; i < numOfLastMonthDays; i++) {
+        copyWeek[i] = numOfDaysBeforeMonth + i - numOfLastMonthDays + 1;
+      }
+      return copyWeek;
+      // 最終週は次の月の日付を入れる
+    } else if (index == calendarData.length - 1) {
+      let copyWeek = _.cloneDeep(week);
+      for (let i = 0; i < numOfNextMonthDays; i++) {
+        copyWeek[6 - i] = numOfNextMonthDays - i;
+      }
+      return copyWeek;
+    } else {
+      return week;
+    }
+  });
+
+  // カレンダーのテーブル要素を変更する
+  const calendarHtml = completeCalendarData.map((week, index) => {
+    const days = week.map(
+      (day: React.ReactChild | null, index: React.Key | null) => {
+        return <td key={index}>{day}</td>;
+      },
+    );
+    return <tr key={index}>{days}</tr>;
+  });
+
+  console.log(2, currentDate);
   return (
-    <div>
-      <h1>Hello.</h1>
+    <div className="calendar-table-contents">
+      {/* 月はmomentでは-1されて取得されるので1を追加する */}
+      <div className="calendar-year">{currentDate.get('year')}</div>
+      <div className="calendar-month">{currentDate.get('month') + 1}</div>
+      <table>
+        <tbody>
+          <tr>
+            <th>SUN</th>
+            <th>MON</th>
+            <th>TUE</th>
+            <th>WED</th>
+            <th>THU</th>
+            <th>FRI</th>
+            <th>SAT</th>
+          </tr>
+          {calendarHtml}
+        </tbody>
+      </table>
     </div>
   );
 };
